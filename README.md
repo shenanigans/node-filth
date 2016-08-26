7b8823a341f4afd2d11135c7cc978501e6f81c22
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2016-08-25 17:31:31 -0700

    SafeMap and cleanup

--- README.md
+++ README.md
@@ -2,24 +2,139 @@ filth
 =====
 Yet another common tools library. Suitable for browser use via browserify.
 
-getTypeStr
-----------
-Uses the `Object.prototype.toString` method to quickly generate a lowercase type string sensitive
-to both Arrays and Buffers.
 
-clone
+Installation
+------------
+Works with [Node.js](https://nodejs.org/en/), [Browserify](http://browserify.org/), or directly in
+the browser.
+```shell
+$ npm install filth
+```
+
+
+Usage
 -----
-Creates a "JSON clone" of a reference by recursing Array items and Object own properties. POJOs in
-the input will become Objects and Arrays in the clone.
+Create an Object that can safely be used as a hash table.
+```javascript
+var filth = require ('filth');
+var myMap = new filth.SafeMap();
+console.log (myMap.__proto__); // undefined
+```
 
-merge
------
-Recursively overwrites properties of one Object with those of another Object. When types do not
-match, the new value is accepted. When Objects and Arrays are found, they are recursed. If an Array
-is merged with a longer Array, novel items are appended.
+Get simplified type strings for tricky items.
+```javascript
+filth.typeof (document.createTextNode ('foo'));
+fs.readFile (filename, function (err, buf) {
+    console.log (filth.typeof (buf)); // buffer
+});
+```
 
-Lock
-----
-A basic locking primitive providing an aqcuire/release pattern, and optionally setup/takedown
-functions and a lock timeout. Timeouts are measured before takedown functions occur. When a timeout
-event occurs the takedown function is still called.
+Perform simple JSON manipulations. Note that `clone` will produce SafeMap instances instead of bare
+Objects.
+```javascript
+var alfa = { foo:'nine' };
+var beta = { bar:'ten' };
+var fullDoc = filth.merge (
+    filth.clone (alfa),
+    beta
+);
+console.log (fullDoc.foo) // "nine"
+console.log (fullDoc.bar) // "ten"
+console.log (alfa.bar); // undefined
+console.log (filth.compare (fullDoc, alfa)) // false
+filth.merge (alfa, beta);
+console.log (filth.compare (fullDoc, alfa)) // true
+```
+
+Inherit from parent classes. Mainly useful when building a `browserify` package as the standard
+`util` module is enormous.
+```javascript
+function ParentClass(){}
+function ModifyingClass(){}
+
+// you can create a non-destructive child
+var ChildClass = filth.createChild (
+    ModifyingClass,
+    ParentClass
+);
+
+// or just modify the child directly
+filth.inherit (ModifyingClass, ParentClass);
+```
+
+If you're using `filth` in `Node.js` you might want to check out the LatencyLogger class. It logs
+the duration of a sequence of tasks as integer microseconds.
+```javascript
+var timeLogger = new filth.LatencyLogger();
+
+do_long_task ("task_01");
+timeLogger.latency ("task_01");
+
+do_long_task ("task_02");
+timeLogger.latency ("task_02");
+
+do_long_task ("task_03");
+timeLogger.latency ("task_03");
+
+var latencies = timeLogger.getFinalLatency();
+for (var name in latencies)
+    console.log (
+        name
+      + ': '
+      + latencies[name]
+      + ' microseconds'
+    );
+```
+
+To limit access to a shared resource, consider the `Lock` primitive. It does not include timeouts so
+use responsibly.
+```javascript
+var filth = require ('filth');
+var async = require ('async');
+
+// create a lock for four users
+var taskLock = new filth.Lock (4);
+async.each (tasks, function (task, callback) {
+    taskLock.take (function(){
+        run_long_task (task, function (err) {
+            taskLock.free();
+            callback (err);
+        });
+    });
+}, function (err) {
+    if (err) {
+        // clear unstarted tasks
+        taskLock.clear();
+    }
+
+    // all tasks complete
+});
+```
+
+See [the API Documentation](shenanigans.github.io/node-filth/docs/module/filth/index.html)
+for more information.
+
+
+LICENSE
+-------
+The MIT License (MIT)
+
+Copyright (c) 2016 Kevin "Schmidty" Smith
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- main.js
+++ main.js
@@ -1,17 +1,72 @@
 
 /**     @module filth
-    Simple utility methods.
+    Simple utility methods and primitives. Typecheck with ease, merge and compare Objects, inherit
+    from a class, lock resources, and batch frequent function calls.
+
+    Browserify users frequenty define their own version of `util.inherits` because the full
+    browser-compatible `util` module is quite large and contains a lot of things almost nobody uses.
+    `filth` is a lightweight way to access inheritence without rewriting `inherits` or rolling your
+    own classes.
 */
-module.exports.getTypeStr   = getTypeStr;
-module.exports.clone        = clone;
-module.exports.merge        = merge;
-module.exports.inherit      = inherit;
-module.exports.compare      = compare;
+
+/**     @class SafeMap
+    An Object with no own properties or property name caveats. Much like `new Object (null)` but
+    solves the remaining issues with `__proto__`.
+@argument/Object source
+    @optional
+    An Object to shallow copy into the new `SafeMap` instance.
+*/
+function SafeMap (source) {
+    if (!(this instanceof SafeMap))
+        return new SafeMap (source);
+    if (source) {
+        var keys = Object.keys (source);
+        for (var i=0,j=keys.length; i<j; i++) {
+            var key = keys[i];
+            this[key] = source[key];
+        }
+    }
+}
+SafeMap.prototype = JSON.parse ('{"__proto__":null}');
+SafeMap.prototype.__proto__ = undefined;
+if (Object.defineProperty)
+    Object.defineProperty (SafeMap.prototype, '__proto__', { enumerable:false });
+
+// for use with IE 10 and below, which have no __proto__
+function FakeSafeMap (source) {
+    if (!(this instanceof FakeSafeMap))
+        return new FakeSafeMap (source);
+    if (source) {
+        var keys = Object.keys (source);
+        for (var i=0,j=keys.length; i<j; i++) {
+            var key = keys[i];
+            this[key] = source[key];
+        }
+    }
+}
+FakeSafeMap.prototype = null;
 
 
 /**     @property/Function getTypeStr
-    Gets a proper type name for a reference, in all lowercase.
+    Gets a proper type name for a reference, in all lowercase, by exploiting the native string
+    representation mechanism. Some possible values:
+     * object
+     * array
+     * buffer
+     * string
+     * boolean
+     * number
+     * function
+     * element
+     * textnode
+     * commentnode
+     * nodelist
+     * htmlcollection
+     * htmldocument
 @argument obj
+*/
+/*      @property/Function typeof
+    @alias filth.getTypeStr
 */
 var typeGetter = ({}).toString;
 // browser safing
@@ -19,7 +74,12 @@ var buff;
 try { buff = Buffer; } catch (err) { buff = {}; }
 function getTypeStr (obj) {
     var tstr = typeGetter.apply(obj).slice(8,-1).toLowerCase();
-    if (tstr == 'object' && obj instanceof Buffer) return 'buffer';
+    if (tstr == 'object')
+        if (obj instanceof buff) return 'buffer';
+        else return tstr;
+    if (tstr == 'text') return 'textnode';
+    if (tstr == 'comment') return 'commentnode';
+    if (tstr.slice (-7) == 'element') return 'element'
     return tstr;
 }
 
@@ -27,12 +87,14 @@ function getTypeStr (obj) {
 /**     @property/Function clone
     Create a JSON-identical duplicate of a reference with no refs in common.
 @argument obj
+    One argument of any JSON-compatible type to be cloned.
+@returns newObj
+    A new object which is json-identical to `obj` ([compare](.compare) will return `true`) but
+    shares no common Object or Array instances.
 */
 function clone (target) {
     var objType = getTypeStr (target);
     switch (objType) {
-        case 'function':
-            throw new Error ('cannot clone Functions');
         case 'undefined':
             return undefined;
         case 'number':
@@ -40,15 +102,17 @@ function clone (target) {
         case 'string':
             return String (target); // otherwise you get heap Strings instead of natives... it's weird.
         case 'boolean':
-            return Boolean (target); // otherwise you get heap Strings instead of natives... it's weird.
+            return Boolean (target); // otherwise you get heap Booleans instead of natives... it's weird.
         case 'array':
             return target.slice().map(clone);
         case 'object':
-            var newObj = {};
+            var newObj = new SafeMap();
             var keys = Object.keys(target);
             for (var i=0,j=keys.length; i<j; i++)
                 newObj[keys[i]] = clone (target[keys[i]]);
             return newObj;
+        case 'function':
+            throw new Error ('cannot clone Functions');
         default:
             throw new Error ('cannot clone type "'+objType+'"');
     }
@@ -56,9 +120,11 @@ function clone (target) {
 
 
 /**     @property/Function merge
-    Overwrite properties on an object with those of another object, recursing into Objects and
-    Arrays.
-@argument obj
+    Overwrite properties on an object with those of another object, recursing into Objects. Source
+    Arrays are appended to target Arrays. Whenever `source` and `target` or values found within them
+    have different types, the `source` value overwrites the 'target' value.
+@argument/Object target
+@argument/Object source
 */
 function merge (target, source) {
     var keys = Object.keys (source);
@@ -77,37 +143,23 @@ function merge (target, source) {
         if (type == 'object')
             merge (target[key], val);
         else if (type == 'array')
-            mergeArray (target[key], val);
+            target[key].push.apply (target[key], val);
         else
             target[key] = val;
-    }
-}
-
-function mergeArray (target, source) {
-    for (var i=0,j=source.length; i<j; i++) {
-        var val = source[i];
-        if (i > target.length) {
-            target[i] = val;
-            continue;
-        }
-        var type = getTypeStr (val);
-        if (type != getTypeStr (target[i])) {
-            target[i] = val;
-            continue;
-        }
-        if (type == 'object')
-            merge (target[i], val);
-        else if (type == 'array')
-            mergeArray (target[i], val);
-        else
-            target[i] = val;
     }
 }
 
 
 /**     @property/Function inherit
     Perform a simple prototype inheritence operation to cause the first argument to inherit from the
-    last.
+    last. Unlike [util.inherts]() the child prototype's own properties are copied over, so you can
+    call `inherit` after setting up the prototype on `child` without anything getting left behind.
+@argument/Function child
+    The class which will inherit from `parent`. Its prototype will be replaced with one chained to
+    `parent` and own properties will be transfered over.
+@argument/Function parent
+    The class which will modify `child`. It is used to generate a new prototype for `child` which is
+    chained to `parent` and its ancestors.
 */
 function inherit (child, parent) {
     var dummy = function(){};
@@ -122,8 +174,34 @@ function inherit (child, parent) {
 }
 
 
-/**     @property/Function compare
+/**     @property/Function createChild
+    A non-destructive version of [inherit](.inherit). Creates a **new** class from `child` which
+    inherits from `parent`.
+@argument/Function child
+    The class which will be copied before inheriting from `parent`.
+@argument/Function parent
+    The class which will modify `child`.
+*/
+function createChild (child, parent) {
+    var dummy = function(){};
+    dummy.prototype = parent.prototype;
+    dummy = new dummy();
+    var keys = Object.keys (child.prototype);
+    for (var i=0,j=keys.length; i<j; i++) {
+        var key = keys[i];
+        dummy[key] = child.prototype[key];
+    }
+    var newChild = function(){ child.apply (this, arguments); };
+    newChild.prototype = dummy;
+    return newChild;
+}
 
+
+/**     @property/Function compare
+    Determine whether two JSON Objects are identical. Object key ordering is not considered.
+@argument/Object objA
+@argument/Object objB
+@returns/Boolean
 */
 function compare (objA, objB) {
     var keys = Object.keys (objA);
@@ -152,6 +230,13 @@ function compare (objA, objB) {
     return true;
 }
 
+/**     @property/Function compareArrays
+    Determine whether two JSON Arrays are recursively identical. Key ordering in child Objects is
+    not considered.
+@argument/Array arrA
+@argument/Array arrB
+@returns/Boolean
+*/
 function compareArrays (arrA, arrB) {
     for (var i=0,j=arrA.length; i<j; i++) {
         var itemA = arrA[i];
@@ -173,4 +258,141 @@ function compareArrays (arrA, arrB) {
             return false;
     }
     return true;
+}
+
+/**     @property/Function hrDiff
+    Return the difference between two [high-resolution timestamps](process.hrtime) as integer
+    microseconds. This is suitable for latencies up to roughly 285 years 224 days.
+*/
+function hrDiff (start, end) {
+    var micro = Math.floor (( end[1] - start[1] ) / 1000 );
+    micro += 1000000 * ( end[0] - start[0] );
+    return micro;
+}
+
+/**     @property/class LatencyLogger
+    An easy logging tool for tracking the myriad latencies of a long serial process and reporting
+    them all at once.
+*/
+function LatencyLogger (latencies) {
+    this.start = process.hrtime();
+    this.latencies = latencies || {};
+    this.latencies.total = 0;
+}
+
+/**     @member/Function latency
+    Log a latency number in integer microseconds relative to the last latency logged. Latencies can
+    **not** be overwitten. Call with no `name` to set the start time for the next logged latency
+    without logging anything this time.
+@argument/String name
+    @optional
+    The name under which this latency should be logged. If no name is passed, or the passed name has
+    already been used, no latency is logged but the start time of the next logged latency is still
+    updated.
+
+    Note that any latency logged as `total` will be overwritten when [getFinalLatency]
+    (#getFinalLatency) is called.
+returns/Boolean written
+    Whether a latency value was stored. `false` when a name conflict occurs.
+*/
+LatencyLogger.prototype.latency = function (name) {
+    var then = this.latencyTime;
+    var now = this.latencyTime = process.hrtime();
+    if (!name || Object.hasOwnProperty.call (this.latencies, name))
+        return false;
+    this.latencies[name] = filth.hrDiff (then, now);
+    return true;
+};
+
+
+/**     @member/Function getFinalLatency
+    Generate the final latency figure relative to a provided initial time, add it to the latencies
+    under the property name `total`, then return the entire latency map.
+@returns/Object<String, Number> latencies
+    A map of logged latency names against the integer microsecond duration of each latency.
+*/
+LatencyLogger.prototype.getFinalLatency = function(){
+    if (!this.latencies.total)
+        this.latencies.total = filth.hrDiff (this.start, process.hrtime());
+    return this.latencies;
+};
+
+/**     @property/class Lock
+    A simple lock for `width` users. Calls to `take` queue up until a slot to process them is
+    available within the lock's width. Does not implement a timeout so it's up to you to worry about
+    ensuring that `free` always gets called.
+@argument/Number width
+    @optional
+    The maximum number of times the lock may be taken in parallel.
+*/
+function Lock (width) {
+    this.width = width || 1;
+    this.taken = 0;
+    this.queue = [];
+    this.isPaused = false;
+};
+
+/**     @member/Function .Lock#take
+    Fires a callback when a slot is available within the [lock width](.Lock(width).
+@callback
+*/
+Lock.prototype.take = function (callback) {
+    if (!this.isPaused && this.taken < this.width) {
+        this.taken++;
+        process.nextTick (callback);
+        return;
+    }
+    this.queue.push (callback);
+};
+
+/**     @member/Function .Lock#free
+    Release the lock, permitting the next callback in the queue to fire.
+*/
+Lock.prototype.free = function(){
+    if (!this.queue.length || this.isPaused)
+        this.taken = Math.max (0, this.taken - 1);
+    else
+        process.nextTick (this.queue.shift());
+};
+
+/**     @member/Function .Lock#pause
+    Stop firing new callbacks from the queue until [play](.Lock#play) is called.
+*/
+Lock.prototype.pause = function(){
+    this.isPaused = true;
+};
+
+/*      @member/Function .Lock#clear
+    Pauses the Lock and deletes all queued callbacks.
+*/
+Lock.prototype.clear = function(){
+    this.isPaused = true;
+    this.queue = [];
+};
+
+/**     @member/Function .Lock#play
+    Start firing new callbacks from the queue again after [pause](.Lock#pause) was called.
+*/
+Lock.prototype.play = function(){
+    this.isPaused = false;
+    while (this.queue.length && this.taken < this.width) {
+        this.taken++;
+        process.nextTick (this.queue.shift());
+    }
+};
+
+module.exports.getTypeStr    = getTypeStr;
+module.exports.typeof        = getTypeStr;
+module.exports.SafeMap       = ({}).__proto__ ? SafeMap : FakeSafeMap;
+module.exports.clone         = clone;
+module.exports.deepCopy      = clone;
+module.exports.merge         = merge;
+module.exports.inherit       = inherit;
+module.exports.createChild   = createChild;
+module.exports.compare       = compare;
+module.exports.compareArrays = compareArrays;
+module.exports.Lock          = Lock;
+if (typeof document === 'undefined') {
+    module.exports.hrDiff        = hrDiff;
+    module.exports.LatencyLogger = LatencyLogger;
 }
